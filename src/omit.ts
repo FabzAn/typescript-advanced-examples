006a778403e2c4b16edb6e1d8578902b75047ca7
Fabian Erdmann <fabian.erdmann@adesso.de>
2022-01-12 14:41:18 +0100

    Add comparison to omit

--- src/omit.ts
+++ src/omit.ts
@@ -9,6 +9,15 @@ type FData = {
   a: string;
   b: Date; // Differs from BData.b
   c: string[]; // Missing on BData
+  // d: number;
+};
+
+// This should be infered
+type TargetType = {
+  a: string;
+  b: Date;
+  c?: never;
+  d?: never;
 };
 
 type FKeys = Exclude<keyof FData, keyof BData>;
@@ -17,12 +26,17 @@ const omitBackendProps: (
   data: FData
 ) => Omit<FData, FKeys> & { [k in FKeys]?: never } = omit(["c"]);
 
+const compareOmitBackendProps = omit(["c"]);
+
 // #############################################################################
 
 const testFData: FData = {
   a: "",
   b: new Date(),
   c: [""],
+  // d: 1,
 };
 
 const omitted = omitBackendProps(testFData);
+
+const compareOmitted = compareOmitBackendProps(testFData);
